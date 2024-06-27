c0338f80fdbcd7aa446304dfb7fc333b3449e83f
kon78 <41447958+kon78@users.noreply.github.com>
2024-06-27 11:09:28 +0300

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_SkillboxSearchEngine_FILE C:/develop/skill_project/build/SkillboxSearchEngine.exe)
-set(__QT_DEPLOY_TARGET_SkillboxSearchEngine_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
